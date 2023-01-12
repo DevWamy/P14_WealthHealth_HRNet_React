2083e44939d54f657a917eb157eee5e29dfcf554
Nadia Bolivant <chelseasama@gmail.com>
2023-01-12 10:11:32 +0100

    Homepage ok

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 // import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { employeeActions } from '../store/employeeSlice';
@@ -7,19 +8,33 @@ const EmployeeForm = () => {
     // const navigate = useNavigate();
     const dispatch = useDispatch();
 
+    //Form values
+    const [updateFirstName, setUpdateFirstName] = useState('');
+    const [updateLastName, setUpdateLastName] = useState('');
+    const [updateDateOfBirth, setUpdateDateOfBirth] = useState('');
+    const [updateStartDate, setUpdateStartDate] = useState('');
+    const [updateStreet, setUpdateStreet] = useState('');
+    const [updateCity, setUpdateCity] = useState('');
+    const [updateState, setUpdateState] = useState('');
+    const [updateZipCode, setUpdateZipCode] = useState('');
+    const [updateDepartment, setUpdateDepartment] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        // const form = e.target;
-        // const firstName = form.querySelector('#firstName').value;
-        // const lastName = form.querySelector('#lastName').value;
-        // const dateOfBirth = form.querySelector('#dateOfBirth').value;
-        // const startDate = form.querySelector('#startDate').value;
-        // const street = form.querySelector('#street').value;
-        // const city = form.querySelector('#city').value;
-        // const state = form.querySelector('#state').value;
-        // const zipCode = form.querySelector('#zipCode').value;
-        // const department = form.querySelector('#department').value;
-        dispatch(employeeActions.addEmployee([]));
+
+        dispatch(
+            employeeActions.addEmployee({
+                firstName: updateFirstName,
+                lastName: updateLastName,
+                dateOfBirth: updateDateOfBirth,
+                startDate: updateStartDate,
+                street: updateStreet,
+                city: updateCity,
+                state: updateState,
+                zipCode: updateZipCode,
+                department: updateDepartment,
+            }),
+        );
     };
 
     return (
@@ -27,42 +42,87 @@ const EmployeeForm = () => {
             <form className="form" onSubmit={handleSubmit}>
                 <div className="inputWrapper">
                     <label htmlFor="firstName">First Name</label>
-                    <input type="text" id="firstName" name="firstName" />
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        onChange={(e) => setUpdateFirstName(e.target.value)}
+                    />
                 </div>
                 <div className="inputWrapper">
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" id="lastName" name="lastName" />
+                    <input
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        onChange={(e) => setUpdateLastName(e.target.value)}
+                    />
                 </div>
                 <div className="inputWrapper">
                     <label htmlFor="dateOfBirth">Date of Birth</label>
-                    <input type="text" id="dateOfBirth" name="dateOfBirth" />
+                    <input
+                        type="text"
+                        id="dateOfBirth"
+                        name="dateOfBirth"
+                        onChange={(e) => setUpdateDateOfBirth(e.target.value)}
+                    />
                 </div>
                 <div className="inputWrapper">
                     <label htmlFor="startDate">Start Date</label>
-                    <input type="text" id="startDate" name="startDate" />
+                    <input
+                        type="text"
+                        id="startDate"
+                        name="startDate"
+                        onChange={(e) => setUpdateStartDate(e.target.value)}
+                    />
                 </div>
                 <fieldset className="address">
                     <legend>Address</legend>
                     <div className="inputWrapper">
                         <label htmlFor="street">Street</label>
-                        <input type="text" id="street" name="street" />
+                        <input
+                            type="text"
+                            id="street"
+                            name="street"
+                            onChange={(e) => setUpdateStreet(e.target.value)}
+                        />
                     </div>
                     <div className="inputWrapper">
                         <label htmlFor="city">City</label>
-                        <input type="text" id="city" name="city" />
+                        <input
+                            type="text"
+                            id="city"
+                            name="city"
+                            onChange={(e) => setUpdateCity(e.target.value)}
+                        />
                     </div>
                     <div className="inputWrapper">
                         <label htmlFor="state">State</label>
-                        <input type="text" id="state" name="state" />
+                        <input
+                            type="text"
+                            id="state"
+                            name="state"
+                            onChange={(e) => setUpdateState(e.target.value)}
+                        />
                     </div>
                     <div className="inputWrapper">
                         <label htmlFor="zipCode">Zip Code</label>
-                        <input type="number" id="zipCode" name="zipCode" />
+                        <input
+                            type="number"
+                            id="zipCode"
+                            name="zipCode"
+                            onChange={(e) => setUpdateZipCode(e.target.value)}
+                        />
                     </div>
                 </fieldset>
                 <div className="inputWrapper">
                     <label htmlFor="department">Department</label>
-                    <input type="text" id="department" name="department" />
+                    <input
+                        type="text"
+                        id="department"
+                        name="department"
+                        onChange={(e) => setUpdateDepartment(e.target.value)}
+                    />
                 </div>
                 <button className="save" type="submit">
                     Save
